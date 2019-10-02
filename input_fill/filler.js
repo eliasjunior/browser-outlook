5ec326d48c653ed0a2df045de2ec6b1c7ad33e60
Elias Moreira <elias_moreirajunior@mastercard.com>
2019-10-02 21:32:42 +0100

    first commit

---
+++ input_fill/filler.js
@@ -0,0 +1,31 @@
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//   // No tabs or host permissions needed!
+//     chrome.tabs.executeScript(null, {file: "content_script.js"});
+// });
+
+// chrome.tabs.executeScript(null, {file: "content_script.js"});
+
+// chrome.commands.onCommand.addListener(function(command) {
+//   chrome.tabs.create({url: "http://www.google.com/"});
+// });
+
+// chrome.browserAction.setBadgeText({text: "ON"});
+
+// run this code below in the console and copy the result
+// 'Yoda3456'.split('').map(item => item.charCodeAt(0)).toString()
+// *****
+
+
+
+(function fillIt() {
+    console.log("should load the fields, all copyright to the master of the universe")
+
+    document.getElementById('field-1').value = "e048578"
+
+    document.getElementById('field-2').value = String.fromCharCode(49,50,51,52,76,117,107,101);
+
+    document.getElementById('field-5').value = String.fromCharCode(50, 57, 49, 48);
+
+    document.getElementById('field-5').focus();
+
+  })();
